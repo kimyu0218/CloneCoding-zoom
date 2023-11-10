ce39133da745bc0781dddf7e4c7b3c23ae85fa85
kimyu0218 <serena35@ewhain.net>
2023-11-10 22:57:59 +0900

    Feat: static 폴더 및 템플릿 엔진 설정
    - static 폴더 public으로 설정
    - 템플릿 엔진으로 pug를 사용
    - view 파일들은 views 하위 디렉토리 내에 존재

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.listen(3000);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000"));
