b9cf24b2cd4845faf2d23b68e86ac140a0440186
kimyu0218 <serena35@ewhain.net>
2023-11-11 21:27:05 +0900

    Feat: 실시간 화상 커뮤니케이션 구현
    - connection : 서버에 소켓이 접속하면 발생하는 이벤트
    - offer : 피어 A가 offer를 전송하면 발생하는 이벤트
      - 해당 room의 모든 피어에게 offer 전달
    - answer : 피어 B가 answer를 전송하면 발생하는 이벤트
      - 해당 room의 모든 피어에게 answer 전달
    - ice : icecandidate 이벤트가 발생하여 candidate를 전송하면 발생하는
      이벤트
      - 해당 room의 모든 피어에게 candidate 전달

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
 dotenv.config();
 
 const app = express();
@@ -16,64 +15,25 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (_, res) => res.render("home"));
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-instrument(io, { auth: false });
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (!sids.get(key)) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(room) {
-  return io.sockets.adapter.rooms.get(room)?.size;
-}
+const io = new Server(server);
 
 io.on("connection", (socket) => {
-  socket["nick"] = "anonymous";
-  io.sockets.emit("room_change", publicRooms());
-
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-
-  socket.on("enter_room", (room, done) => {
+  socket.on("join_room", (room) => {
     socket.join(room);
-    done();
-    socket.to(room).emit("welcome", socket.nick, countRoom(room));
-    io.sockets.emit("room_change", publicRooms());
+    socket.to(room).emit("welcome");
   });
 
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nick, countRoom(room) - 1)
-    );
+  socket.on("offer", (offer, room) => {
+    socket.to(room).emit("offer", offer);
   });
 
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
+  socket.on("answer", (answer, room) => {
+    socket.to(room).emit("answer", answer);
   });
 
-  socket.on("chat", (msg, room, done) => {
-    socket.to(room).emit("chat", `[${socket.nick}]: ${msg}`);
-    done();
+  socket.on("ice", (ice, room) => {
+    socket.to(room).emit("ice", ice);
   });
-
-  socket.on("nick", (nickname) => (socket["nick"] = nickname));
 });
 
 server.listen(process.env.port, () =>
