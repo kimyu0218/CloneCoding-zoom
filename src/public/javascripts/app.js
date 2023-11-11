b8889900b01c2a2e3a87d66f6716db18edc45318
kimyu0218 <serena35@ewhain.net>
2023-11-11 23:59:56 +0900

    Feat: Data Channel 추가
    - Offer를 생성하는 피어 A에서 createDataChannel() 호출
    - Answer를 생성하는 피어 B에서 데이터채널 이벤트가 발생했을 때 데이터
      채널 할당
    - 양방향 통신을 위해 양측 모두 데이터 채널에 message 이벤트 리스너 등록

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -12,6 +12,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -123,12 +124,20 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // socket
 
 socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", (event) => console.log(event.data));
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   socket.emit("offer", offer, roomName);
 });
 
 socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", (event) =>
+      console.log(event.data)
+    );
+  });
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
   myPeerConnection.setLocalDescription(answer);
@@ -155,7 +164,19 @@ function handleAddStream(data) {
 }
 
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
